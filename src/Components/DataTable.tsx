e0577ea82d7d5965310f0cf9494d4d7573e69c3f
jaemin <als982001@naver.com>
2025-02-16 16:41:16 +0900

    디자인 대략 수정

--- src/Components/DataTable.tsx
+++ src/Components/DataTable.tsx
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 interface IProps {
   selectedFieldLabels: Record<string, string>;
   selectedData: IProduct[] | IGameCharacter[] | ICustomerOrder[];
@@ -7,38 +9,45 @@ export default function DataTable({
   selectedFieldLabels,
   selectedData,
 }: IProps) {
-  const lables = Object.keys(selectedFieldLabels);
+  const labels = Object.keys(selectedFieldLabels);
+  const columnNums = labels.length;
+  const rowNums = selectedData.length + 1;
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        width: "100%",
-      }}
-    >
-      <div style={{ display: "flex", width: "100%" }}>
-        {lables.map((label) => (
-          <div key={label}>
-            <span>
-              {selectedFieldLabels[label as keyof typeof selectedFieldLabels]}
-            </span>
-          </div>
-        ))}
-      </div>
-      <div style={{ display: "flex", width: "100%" }}>
-        {selectedData.map((datum) => {
-          return (
-            <div key={datum.id}>
-              {lables.map((label) => (
-                <div key={label}>
-                  <span>{datum[label as keyof typeof datum]}</span>
-                </div>
-              ))}
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    <Container rowNums={rowNums} columnNums={columnNums}>
+      {labels.map((label) => (
+        <Cell style={{ borderBottom: "1px solid black" }} key={label}>
+          <span>
+            {selectedFieldLabels[label as keyof typeof selectedFieldLabels]}
+          </span>
+        </Cell>
+      ))}
+      {selectedData.map((datum) => {
+        return (
+          <>
+            {labels.map((label) => (
+              <Cell key={label}>
+                <span>{datum[label as keyof typeof datum]}</span>
+              </Cell>
+            ))}
+          </>
+        );
+      })}
+    </Container>
   );
 }
+
+const Container = styled.div<{ columnNums: number; rowNums: number }>`
+  width: 100%;
+  display: grid;
+  grid-template-rows: ${({ rowNums }) => `repeat(${rowNums}, 1fr)`};
+  grid-template-columns: ${({ columnNums }) => `repeat(${columnNums}, 1fr)`};
+  margin-left: 30px;
+`;
+
+const Cell = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  height: 30px;
+`;
